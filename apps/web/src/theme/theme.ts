0a3874844c3693b424990a744c2e328f115baa08
KadoProG <splatrain@gmail.com>
2024-01-30 07:56:06 +0900

    ダークモードに対応

--- apps/web/src/theme/theme.ts
+++ apps/web/src/theme/theme.ts
@@ -1,12 +1,23 @@
 import { createTheme } from '@mui/material';
 
-export const theme = createTheme({
+export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
       main: '#EFE2CD',
-      // light: '#ffa2a3',
-      // dark: '#a34449',
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#a98c5e',
+    },
+    background: {
+      default: '#333',
+      paper: '#111',
     },
   },
 });
--- apps/web/src/theme/themeRegistry.tsx
+++ apps/web/src/theme/themeRegistry.tsx
@@ -1,13 +1,44 @@
 'use client';
 
+import { useMediaQuery } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import * as React from 'react';
-import { theme } from '@/theme/theme';
+import { darkTheme, lightTheme } from '@/theme/theme';
 
-export const ThemeRegistry = (props: { children: React.ReactNode }) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    {props.children}
-  </ThemeProvider>
-);
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+export const ThemeRegistry = (props: { children: React.ReactNode }) => {
+  const prefersInit = useMediaQuery('(prefers-color-scheme: dark)')
+    ? 'dark'
+    : 'light';
+
+  const [mode, setMode] = React.useState<'light' | 'dark'>(prefersInit);
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
+  React.useEffect(() => {
+    setMode(prefersInit);
+  }, [prefersInit]);
+
+  const theme = React.useMemo(
+    () => (mode === 'light' ? lightTheme : darkTheme),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
