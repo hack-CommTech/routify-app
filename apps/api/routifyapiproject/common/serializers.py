01fb13dd10d477708a44c521642e83e37319cb33
takuyakubo <takuyakubo7140@gmail.com>
2024-02-03 04:57:59 +0900

    add:シリアライザーを定義しました。

--- apps/api/routifyapiproject/common/serializers.py
+++ apps/api/routifyapiproject/common/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Habit
-        fields = {'id', 'habit_name', 'start_time', 'end_time', 'is_done', 'user'}
+        fields = {'id', 'habit_name', 'description', 'clear_judge', 'is_require_elapsedtime','is_include_holiday', 'schedule_routine', 'user'}
         read_only_fields = ('id')
 
 
