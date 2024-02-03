b0f01f57ebda1a003bc298f74ede73f88ec3125f
takuyakubo <takuyakubo7140@gmail.com>
2024-02-03 11:46:15 +0900

    add:接続テスト

--- apps/api/routifyapiproject/common/models.py
+++ apps/api/routifyapiproject/common/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=64)
+    user_Name = models.CharField(max_length=64)
     user_email = models.EmailField()
     user_password = models.CharField(max_length=1024)
 
     def __str__(self):
-        return self.user_name
+        return self.user_Name
 
     class Meta:
         db_table = 'user_table'
