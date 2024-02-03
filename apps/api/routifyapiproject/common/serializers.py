7cf87b5405ebd23710926ac3c041d15bdcdd1a2e
takuyakubo <takuyakubo7140@gmail.com>
2024-02-03 14:19:58 +0900

    update:データ取得エラーを修正

--- apps/api/routifyapiproject/common/serializers.py
+++ apps/api/routifyapiproject/common/serializers.py
@@ -5,19 +5,19 @@ from . import models
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = {'id', 'user_name', 'user_email', 'user_password'}
+        fields = ('id', 'user_name', 'user_email', 'user_password')
         read_only_fields = ('id')
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Habit
-        fields = {'id', 'habit_name', 'description', 'clear_judge', 'is_require_elapsedtime','is_include_holiday', 'schedule_routine', 'user'}
-        read_only_fields = ('id')
+        fields = ('id', 'habit_name', 'description', 'clear_judge', 'is_require_elapsedtime','is_include_holiday', 'schedule_routine', 'user')
+        # read_only_fields = ('id')
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Schedule
-        fields = {'id', 'start_time', 'end_time', 'description', 'is_done', 'habit'}
+        fields = ('id', 'start_time', 'end_time', 'description', 'is_done', 'habit')
         read_only_fields = ('id')
